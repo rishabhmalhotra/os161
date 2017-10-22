b4ea560a12a39047112195ab8aa41c73eba9f251
Rishabh Malhotra <malhotra.rishabh88@yahoo.com>
2017-10-22 00:12:28 -0400

    efef

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -9,7 +9,8 @@
 #include <thread.h>
 #include <addrspace.h>
 #include <copyinout.h>
-//#include "opt-A2.h"
+#include <spinlock.h>
+#include "opt-A2.h"
 
   /* this implementation of sys__exit does not do anything with the exit code */
   /* this needs to be fixed to get exit() and waitpid() working properly */
