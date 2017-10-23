94b2a0ab5eedbbf8c29c4a116770b4a96668760d
Rishabh Malhotra <malhotra.rishabh88@yahoo.com>
2017-10-23 00:05:25 -0400

    script generated message: debug, fixing bugs

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -136,12 +136,12 @@ sys_fork(void *tf) {
   void *heaptf = kmalloc(sizeof(*tf));                                           // heaptf is (parent) tf on the heap
   heaptf = tf;
   memcpy(heaptf,tf, sizeof(*tf));
-  int err_no = thread_fork("childThread", childproc, enter_forked_process(heaptf, data2), heaptf, data2);
+  int err_no = thread_fork(curthread->t_name, childproc, &enter_forked_process(heaptf, data2), heaptf, data2);
   if (err_no) {
     proc_destroy(childproc);
     kfree(heaptf);
     heaptf = NULL;
-    return err;
+    return err_no;
   }
 
   // add childproc to the children array of its parent
