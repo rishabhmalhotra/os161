64e99beb812bcfa3cca139000ab81e6d026cea54
Rishabh Malhotra <malhotra.rishabh88@yahoo.com>
2017-10-25 20:43:18 -0400

    script generated message: debug, fixing bugs

--- kern/include/syscall.h
+++ kern/include/syscall.h
@@ -67,7 +67,7 @@ void sys__exit(int exitcode);
 int sys_getpid(pid_t *retval);
 int sys_waitpid(pid_t pid, userptr_t status, int options, pid_t *retval);
 #if OPT_A2
-int sys_fork(void *tf, pid_t *retval);
+int sys_fork(struct trapframe *tf, pid_t *retval);
 #endif
 
 #endif // UW
