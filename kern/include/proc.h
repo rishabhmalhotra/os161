74f4a978893a0ed4a0af5f52e8be2556272066f1
Rishabh Malhotra <malhotra.rishabh88@yahoo.com>
2017-10-28 10:57:31 -0400

    script generated message: debug, fixing bugs

--- kern/include/proc.h
+++ kern/include/proc.h
@@ -64,8 +64,8 @@ struct procTable {
 	int state;							/* zombie, orphan,.... */
     int exitCode;
 };
- struct lock *procTableLock;
- struct cv *procTableW8Cv;
+struct lock *procTableLock;
+struct cv *procTableW8Cv;
 
 #endif
 
