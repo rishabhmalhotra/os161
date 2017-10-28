ae261a6fff2b98e125e6e7af242bd3f71ac9d50a
Rishabh Malhotra <malhotra.rishabh88@yahoo.com>
2017-10-27 20:54:48 -0400

    script generated message: debug, fixing bugs

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -112,9 +112,9 @@ sys_waitpid(pid_t pid,
   struct proc *p = NULL;
   // check all currently alive processes to see if a proc with this PID exists:
   for (unsigned int i=0; i<array_num(aliveProcs); i++) {
-    if (array_get(aliveProcs, i) != NULL) {
-      if (array_get(aliveProcs, i)->pid == pid) {
-        p = array_get(aliveProcs, i);
+    if ((struct proc *)array_get(aliveProcs, i) != NULL) {
+      if ((struct proc *)array_get(aliveProcs, i)->pid == pid) {
+        p = (struct proc *)array_get(aliveProcs, i);
       }
     }
   }
