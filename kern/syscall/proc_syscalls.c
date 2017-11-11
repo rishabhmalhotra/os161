7629dfb5cdf679c8fece4dcf75b484823b90d3c0
Rishabh Malhotra <malhotra.rishabh88@yahoo.com>
2017-11-11 10:45:21 -0500

    script generated message: debug, fixing bugs

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -16,6 +16,7 @@
 #include <synch.h>
 #include <vfs.h>
 #include <vm.h>
+#include <kern/fcntl.h>
 
   /* this implementation of sys__exit does not do anything with the exit code */
   /* this needs to be fixed to get exit() and waitpid() working properly */
