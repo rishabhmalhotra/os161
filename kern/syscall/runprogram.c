1611efe7fe1fd0e0cc9f36b689ae8e330b2c5e00
Rishabh Malhotra <malhotra.rishabh88@yahoo.com>
2017-11-17 18:47:00 -0500

    script generated message: debug, fixing bugs

--- kern/syscall/runprogram.c
+++ kern/syscall/runprogram.c
@@ -102,8 +102,6 @@ runprogram(char *progname, char **args, int nargs)
 
 	#if OPT_A2
 
-	kprintf("Hello\n");
-
 	vaddr_t arrayOfStackAddress[nargs];
   	// NULL terminate
   	arrayOfStackAddress[nargs] = 0;
@@ -117,6 +115,7 @@ runprogram(char *progname, char **args, int nargs)
 
   		// put onto userspace from kern space:
   		result = copyoutstr(args[i], (userptr_t)stackptr, argLen, NULL);
+  		kprintf("Hello1[%d]\n", i);
 
   		if (result) return result;
 
@@ -129,6 +128,7 @@ runprogram(char *progname, char **args, int nargs)
   	for (int i=(nargs-1); i>=0; i--) {
   		stackptr -= ROUNDUP(sizeof(vaddr_t), 4);											// round to 4 bytes as in ass. spec
   		result = copyout(&arrayOfStackAddress[i], (userptr_t)stackptr, sizeof(vaddr_t));
+  		kprintf("Hello2[%d]\n", i);
   		if (result) return result;
   	}
 
