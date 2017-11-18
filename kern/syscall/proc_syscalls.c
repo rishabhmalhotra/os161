4bd02bfc6e1466d6a3742f5ed2e686580a5b02cf
Rishabh Malhotra <malhotra.rishabh88@yahoo.com>
2017-11-17 20:05:42 -0500

    script generated message: debug, fixing bugs

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -262,6 +262,8 @@ int sys_execv(const userptr_t program, userptr_t args) {
       	}
 	}
 
+	kprintf("numArgs: %d\n", numArgs);
+
 	// put each of args[] onto kernel space which will be held inside kernArgs[]
 	char **kernArgs = kmalloc((numArgs+1) * sizeof(char *));
 	if (kernArgs == NULL) return ENOMEM;
@@ -271,7 +273,7 @@ int sys_execv(const userptr_t program, userptr_t args) {
 
 	for (int i=0; i<numArgs; i++) {
 		int kernArgLen = strlen(kernArgs[i]) + 1;
-		kernArgs[i] = kmalloc(kernArgLen * sizeof (char *));////////////////////
+		kernArgs[i] = kmalloc(kernArgLen * sizeof (char));			///////////////////////////////////////
 		// put args[i] from userspace into kernArgs[i] ie onto kernel space (kernArgLen bytes; including NULL terminator)
 		char *temp = NULL;
       	copyin(args + (i * sizeof(char *)), &temp, sizeof(char *));
