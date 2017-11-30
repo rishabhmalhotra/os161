6c01f6711da790398eb87aa5a9bace6e738265a9
Rishabh Malhotra <malhotra.rishabh88@yahoo.com>
2017-11-30 12:12:41 -0500

    resolved errors for coremap

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -436,9 +436,9 @@ void
 as_destroy(struct addrspace *as)
 {
 	#if OPT_A3
-	kfree(as->as_pbase1);
-	kfree(as->as_pbase2);
-	kfree(as->as_stackpbase);
+	kfree((vaddr_t)as->as_pbase1);
+	kfree((vaddr_t)as->as_pbase2);
+	kfree((vaddr_t)as->as_stackpbase);
 	#endif	// OPT_A3
 	kfree(as);
 }
