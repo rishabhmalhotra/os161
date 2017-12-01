0b617b737b43bb90c6c2ba2cc20dae197d10bdee
Rishabh Malhotra <malhotra.rishabh88@yahoo.com>
2017-12-01 11:30:30 -0500

    fixes

--- kern/arch/mips/vm/dumbvm.c
+++ kern/arch/mips/vm/dumbvm.c
@@ -96,7 +96,9 @@ vm_bootstrap(void)
 
 	// keep track of which frame is free:
 	// has 1 entry for each frame (as an array)
-	coreMap->coreMapMappingAndFrameInfo = (struct coreMapMappingAndFrameInfo*) PADDR_TO_KVADDR(lo);
+	kprintf("lol\n");
+	coreMap = (struct coreMap*) PADDR_TO_KVADDR(lo);
+	kprintf("lolafter\n");
 
 	// find space for coreMap structure:
 	lo += numberOfFrames * (sizeof (struct coreMap));
